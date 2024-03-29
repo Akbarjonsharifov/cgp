3d45fc5b473bd7a30aab07d60dd10865912cdfa7
Akbar Sharifov <akbarjon.sharifov01@gmail.com>
2024-03-29 03:35:02 +0000

    final week3 done

--- Week3/Form1.cs
+++ Week3/Form1.cs
@@ -11,39 +11,69 @@ namespace Week3
         Rectangle aRect;
         Rectangle anEllipse;
         Rectangle moving;
+        System.Windows.Forms.Timer timer;
         int x = 0, y = 0;
-        Graphics g;
+        bool isMovingDownRight = true;
+        //Graphics g;
         public Form1()
         {
             InitializeComponent();
 
-            // set up the rectangle objects using client (form) coordinates
+            
             aRect = new Rectangle(100, 100, 200, 200);
             anEllipse = new Rectangle(150, 150, 200, 100);
             moving = new Rectangle(x, y, 10, 10);
-            // size and position the frame
+            
             this.StartPosition = FormStartPosition.Manual;
             this.Location = new Point(0, 0);
             this.SetStyle(ControlStyles.ResizeRedraw, true);
             this.Width = 500;
             this.Height = 500;
             this.BackColor = Color.White;
+
+            timer = new System.Windows.Forms.Timer
+            {
+                Interval = 10
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (isMovingDownRight)
+            {
+                x++;
+                y++;
+            }
+            else
+            {
+                x--;
+                y--;
+            }
+
+            if (x > this.Width - moving.Width || y > this.Height - moving.Height || x < 0 || y < 0)
+            {
+                isMovingDownRight = !isMovingDownRight;
+            }
+
+            moving.Location = new Point(x, y);
+            this.Invalidate();
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            g = e.Graphics;
-            Brush redBrush = new SolidBrush(Color.Red);
-            g.FillRectangle(redBrush, aRect);
-            Brush greenBrush = new SolidBrush(Color.Green);
-            g.FillEllipse(greenBrush, anEllipse);
-        }
-    }
-    public class XORDemo
-    {
-        public static void Main ()
-        {
-            Application.Run(new Form1());
+            base.OnPaint(e);
+            using (Graphics g = e.Graphics)
+            {
+
+                using (Brush movingBrush = new SolidBrush(Color.FromArgb(255, 255 - (x % 256), 255 - (y % 256))))
+                {
+                    g.FillRectangle(movingBrush, moving);
+                }
+            }
         }
     }
 }
+
+
