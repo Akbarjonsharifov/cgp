06d2aa72c3ba1e601a4fd30ef03ccebdb3f2b24a
Akbar Sharifov <akbarjon.sharifov01@gmail.com>
2024-04-04 21:59:57 +0100

    week7 unfinished

---
+++ week6/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week6
+{
+    public partial class Form1 : Form
+    {
+        public class Matrix2D
+        {
+            private int[,] matrix;
+
+            public Matrix2D(int[,] initialMatrix)
+            {
+                if (initialMatrix.GetLength(0) == 2 && initialMatrix.GetLength(1) == 2)
+                {
+                    matrix = initialMatrix;
+                }
+                else
+                {
+                    throw new ArgumentException("The Matrix must be 2*2");
+                }
+
+            }
+
+            public void DisplayMatrix() //displaying matrix 
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+
+}
---
+++ week6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace week6
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+
+            // Start the Windows Form application
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            // Define a 2x2 matrix for testing
+            int[,] myMatrix = { { 1, 2 }, { 3, 4 } };
+
+            // Create an instance of Matrix2D with the test matrix
+            Form1.Matrix2D matrix = new Form1.Matrix2D(myMatrix);
+
+            // Display the matrix in the console for testing purposes
+            Console.WriteLine("Displaying 2x2 Matrix:");
+            matrix.DisplayMatrix();
+
+            // Wait for input to continue (so you can see the output before the form opens)
+            Console.WriteLine("Press any key to continue...");
+            //Console.ReadKey();
+        }
+    }
+}
